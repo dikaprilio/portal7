332a7a9b377729e353856ec52cf770603bda3fc1
Diraha <diazharahap25@gmail.com>
2026-06-15 21:06:27 +0700

    Nyoba Building

--- src/media-content.js
+++ src/media-content.js
@@ -3,7 +3,7 @@ import photo1 from 'url:./img/Slide1.jpg';
 import photo2 from 'url:./img/Slide2.jpg';
 import photo3 from 'url:./img/Slide3.jpg';
 import photo4 from 'url:./img/Slide4.jpg';
-import photo5 from 'url:./img/Slide5.jpg';
+import photo5 from 'url:./img/Slide5.jpeg';
 import photo6 from 'url:./img/IMG_3376.jpg';
 import photo7 from 'url:./img/IMG_3104.jpg';
 
